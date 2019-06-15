774af521707f407151b643fc9b34fff177c4b89e
sebin <hellosebinvincent@gmail.com>
2019-06-15 11:53:51 +0530

    final submit before submission

--- Resumex/settings.py
+++ Resumex/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='hellosebinvincent@gmail.com'
-EMAIL_HOST_PASSWORD='xxxxxxxxxx'
+EMAIL_HOST_PASSWORD='xxxxxxxxxxxx'
 EMAIL_PORT=587
 EMAIL_USE_TLS= True
 
